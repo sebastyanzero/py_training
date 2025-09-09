e280d499f5b62c2ae3504bd5f9436bdb7084b8a8
knyaz3akat <s.n.golubov@gmail.com>
2025-09-10 01:34:07 +0300

    Перевели ТК на pytest и вынесли в класс фикстуры функции

--- test_add_contact.py
+++ test_add_contact.py
@@ -25,43 +25,4 @@ def test_add_empty_contact(app):
                         sitehpage="", bday="", bmonth="", byear="", aday="", amonth="", ayear=""))
     app.logout()
 
-    """
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main() """
