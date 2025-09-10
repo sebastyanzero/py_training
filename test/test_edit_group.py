56072693607c181826882c650fe1d8aa94f48082
knyaz3akat <s.n.golubov@gmail.com>
2025-09-11 01:32:50 +0300

    Создан ТК для редактирования 1й группы

---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="new1", header="new2", footer="new3"))
+    app.session.logout()
